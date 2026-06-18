0aded577c0bce10c79f00a4f3c1ab073ebaf6220
Himanshu <himanshukori947@gamil.com>
2026-06-18 23:07:46 +0530

    Updated 18-06-2026

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "https://ai-db-idps.onrender.com"
+API_URL = "https://ai-db-idps-77hv.onrender.com"
 
 st.set_page_config(layout="wide")
 st.title("🛡️ AI-DB-IDPS Dashboard")
